de4f4e6ba941be0fb0ef516d14cc0bcb4f36b2b8
zeronedev <ok7375@gmail.com>
2022-03-07 01:28:26 +0900

    #8.6 Home Screen part Two: 타이틀 오버뷰 배경

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,24 @@
 const API_KEY = "0a7bc05450ef72c67f947089a1e46c7d";
 const BASE_PATH = "https://api.themoviedb.org/3";
 
+interface IMovie {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+export interface IGetMoviesResult {
+  dates: {
+    mximum: string;
+    minimum: string;
+  };
+  page: number;
+  results: IMovie[];
+  total_pages: number;
+  total_results: number;
+}
+
 export function getMovies() {
   return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
     (response) => response.json()
